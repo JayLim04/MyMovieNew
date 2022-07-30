ce9b99145514800cf2245bf8f62e6b65b5dedd67
AhmadNoorIfwat <21010771@myrp.edu.sg>
2022-07-30 14:06:54 +0800

    pushed dbhelper and movies(object)

---
+++ app/src/main/java/sg/edu/rp/c346/id21033869/mymovienew/DBHelper.java
@@ -0,0 +1,137 @@
+package sg.edu.rp.c346.id21033869.mymovienew;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "movies.db";
+    private static final int DATABASE_VERSION = 2;
+    private static final String TABLE_MOVIES = "Movies";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_MOVIE_TITLE = "title";
+    private static final String COLUMN_GENRE = "genre";
+    private static final String COLUMN_YEAR = "year";
+    private static final String COLUMN_RATING = "rating";
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createMovieTableSql = "CREATE TABLE " + TABLE_MOVIES + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_MOVIE_TITLE + " TEXT,"
+                + COLUMN_GENRE + " TEXT,"
+                + COLUMN_YEAR + " INTEGER,"
+                + COLUMN_RATING + " INTEGER )";
+        db.execSQL(createMovieTableSql);
+        Log.i("info", "created tables");
+
+        for (int i = 0; i< 4; i++) {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_MOVIE_TITLE, "Data number " + i);
+            db.insert(TABLE_MOVIES, null, values);
+        }
+        Log.i("info", "dummy records inserted");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
+        //onCreate(db);
+        db.execSQL("ALTER TABLE " + TABLE_MOVIES + " ADD COLUMN title TEXT ");
+    }
+
+    public long insertSong(String title, String singers, int  year, int stars) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_MOVIE_TITLE, title);
+        values.put(COLUMN_GENRE, singers);
+        values.put(COLUMN_YEAR,  year);
+        values.put(COLUMN_RATING,stars);
+        long result = db.insert(TABLE_MOVIES, null, values);
+        db.close();
+        Log.d("SQL Insert","ID:"+ result); //id returned, shouldn’t be -1
+        return result;
+    }
+
+    public ArrayList<Movies> getAllMovies(){
+        ArrayList<Movies> movies = new ArrayList<Movies>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {COLUMN_ID, COLUMN_MOVIE_TITLE, COLUMN_GENRE, COLUMN_YEAR, COLUMN_RATING};
+        Cursor cursor = db.query(TABLE_MOVIES, columns, null, null,
+                null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String title = cursor.getString(1);
+                String genre = cursor.getString(2);
+                int year = cursor.getInt(3);
+                String rating = cursor.getString(4);
+                Movies movie = new Movies(id, title, genre, year, rating);
+                movies.add(movie);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return movies;
+    }
+
+    public int updateMovies(Movies data){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_MOVIE_TITLE, data.getTitle());
+        values.put(COLUMN_GENRE, data.getGenre());
+        values.put(COLUMN_YEAR, data.getYear());
+        values.put(COLUMN_RATING, data.getRating());
+        String condition = COLUMN_ID + "= ?";
+        String[] args = {String.valueOf(data.getId())};
+        int result = db.update(TABLE_MOVIES, values, condition, args);
+        db.close();
+        return result;
+    }
+
+    public int deleteMovies(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String condition = COLUMN_ID + "= ?";
+        String[] args = {String.valueOf(id)};
+        int result = db.delete(TABLE_MOVIES, condition, args);
+        db.close();
+        return result;
+    }
+
+    public ArrayList<Movies> getMovieRating(int keyword) {
+        ArrayList<Movies> movies = new ArrayList<Movies>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns= {COLUMN_ID, COLUMN_MOVIE_TITLE, COLUMN_GENRE, COLUMN_YEAR, COLUMN_RATING};
+        String condition = COLUMN_RATING + " Like ?";
+        String[] args = { "%" +  keyword + "%"};
+        Cursor cursor = db.query(TABLE_MOVIES, columns, condition, args,
+                null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String title = cursor.getString(1);
+                String singers = cursor.getString(2);
+                int year = cursor.getInt(3);
+                String rating = cursor.getString(4);
+                Movies movie = new Movies(id, title, singers, year, rating);
+                movies.add(movie);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return movies;
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id21033869/mymovienew/Movies.java
@@ -0,0 +1,62 @@
+package sg.edu.rp.c346.id21033869.mymovienew;
+
+import java.io.Serializable;
+
+import android.os.Bundle;
+
+public class Movies implements Serializable {
+
+    private int id;
+    private String title;
+    private String genre;
+    private int year;
+    private String rating;
+
+    public Movies(int id, String title, String genre, int year, String rating){
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String movie) {
+        this.title = movie;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
